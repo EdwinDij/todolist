cc511c8e2a42e3e64d149b33494503c2e18975fb
EdwinDij <edwin.d899@gmail.com>
2022-07-18 16:53:57 +0200

    function todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
 import './App.css';
-import Textbar from './components/Textbar/Textbar';
+
+
 function App() {
+
+  const [task, setTask] = useState<string>("")
+  const [deadline, setDeadline] = useState<number>(0)
+  const [todo, setTodoList] = useState([]);
+
+const handleChange = () => {
+
+}
   return (
     <div className="App">
-        <Textbar />
+        <div className='header'>
+          <div className="inputContainer">
+          <input type="text" placeholder="Mes taches..." 
+          onChange={handleChange} />
+          <input type="number" placeholder="Temps"
+           onChange={handleChange} />
+          </div>
+          <button>Ajouter une tâche</button>
+        </div>
+        <div className='todoList'></div>
     </div>
   );
 }
